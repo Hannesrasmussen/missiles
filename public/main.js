376fb5e1f31adea7a7ad40855860b9de074bd0e8
hannesrasmussen <hannes.e.rasmussen@gmail.com>
2024-05-04 17:55:43 +0200

    v.3

--- public/main.js
+++ public/main.js
@@ -1,11 +1,13 @@
 const modalWrapper = document.getElementById('modal-wrapper');
 const navItems = document.getElementsByClassName('nav-item');
+const moreGigsBtn = document.getElementById('modal-action');
+const gigsCollapse = document.getElementById('gigs-collapseable');
 
 function init() {
   // Event Listeners
   modalWrapper.addEventListener('click', handleOutsideModalClick);
-
   navItems[3].addEventListener('click', showModal);
+  moreGigsBtn.addEventListener('click', showMoreGigs);
 }
 
 function showModal() {
@@ -14,9 +16,15 @@ function showModal() {
 
 function handleOutsideModalClick(e) {
   console.log(e.currentTarget);
-  if (e.currentTarget.id === 'modal-wrapper') {
+  if (e.target.id === 'modal-wrapper') {
     e.currentTarget.style.display = 'none';
+    gigsCollapse.style.display = 'none';
   }
+}
+
+function showMoreGigs() {
+  console.log(gigsCollapse);
+  gigsCollapse.style.display = 'block';
 }
 
 window.onload = init();
